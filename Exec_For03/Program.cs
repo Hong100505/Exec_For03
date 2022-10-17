9aa12b43d00be1c74033565f68be9d42b573dd2b
ispan <ispan@DESKTOP-PAKSETB>
2022-10-17 16:59:25 +0800

    加入專案檔案。

---
+++ Exec_For03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 5; i++)
+			{
+				int countOfEmpty = 5 - i;
+				int countStar = i * 2 - 1;
+				string row = new string(' ', countOfEmpty) + new string('*', countStar);
+				Console.WriteLine(row);
+			}
+		}
+	}
+}
